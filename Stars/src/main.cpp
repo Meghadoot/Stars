323a831c7a4e655eac4ef213396295feed752170
Meghadoot Gardi <rkomeghadoot@gmail.com>
2020-03-28 21:40:25 +0100

    Create Main(C++) Application

---
+++ Stars/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Stars.h"
+
+
+int main(int argc, char **argv)
+{
+    int d = 5;
+    print_in_row(d);
+    std::cout << "Hello!" << std::endl;
+    
+    return 0;
+}
