33e2f4cab7ffe8dcfaedd69eb5d4f622afaa5d4d
Meghadoot Gardi <rkomeghadoot@gmail.com>
2020-03-29 01:09:02 +0100

    Update IO commands

--- Stars/src/main.cpp
+++ Stars/src/main.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
+#include <iomanip>
+#include <string>
 #include "Stars.h"
 
 
 int main(int argc, char **argv)
 {
-    int d = 5;
-    print_in_row(d);
-    std::cout << "Hello!" << std::endl;
+    /*
+     * Welcome to the Star Galaxy 
+     * Test/Fun Project by MG 
+     * 
+     */
+    int count = 0;
+    std::string temp;
+    std::cout << "Hello !!!\n" << std::endl;
+    std::cout << "*** Welcome to the Star Galaxy ***\n\n\n" << std::endl;
+    //TODO: Takes only number of stars now. Need to take symbol as an input from user 
+    std::cout << "Please, Enter Maximum number of Stars: ";
+    std::getline(std::cin, temp);
+    count = std::stoi(temp);
+    print_in_diagnonal(count);
     
     return 0;
 }
